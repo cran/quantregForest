44ee56488b47328e18e1dff3ae607ad2c283d520
Loris Michel <michel@stat.math.ethz.ch>
2017-11-14 17:07:06 +0000

    version 1.3-6

--- R/quantregForest.R
+++ R/quantregForest.R
@@ -1,13 +1,13 @@
 "quantregForest" <-
 function(x,y, nthreads = 1, keep.inbag=FALSE, ...){
 
-  ## Some checks 
+  ## Some checks
   #if(! class(y) %in% c("numeric","integer") )
   #  stop(" y must be numeric ")
-  
+
   if(is.null(nrow(x)) || is.null(ncol(x)))
     stop(" x contains no data ")
-    
+
 
   if( nrow(x) != length(y) )
     stop(" predictor variables and response variable must contain the same number of samples ")
@@ -15,8 +15,8 @@ function(x,y, nthreads = 1, keep.inbag=FALSE, ...){
   if (any(is.na(x))) stop("NA not permitted in predictors")
   if (any(is.na(y))) stop("NA not permitted in response")
 
-  
-  
+
+
   ## Check for categorial predictors with too many categories (copied from randomForest package)
    if (is.data.frame(x)) {
         ncat <- sapply(x, function(x) if(is.factor(x) && !is.ordered(x))
@@ -28,7 +28,7 @@ function(x,y, nthreads = 1, keep.inbag=FALSE, ...){
     if (maxcat > 32)
         stop("Can not handle categorical predictors with more than 32 categories.")
 
-  
+
   ## Note that crucial parts of the computation
   ## are only invoked by the predict method
   cl <- match.call()
@@ -39,38 +39,61 @@ function(x,y, nthreads = 1, keep.inbag=FALSE, ...){
   }else{
     randomForest( x=x,y=y ,keep.inbag=keep.inbag,...)
   }
-  
+
   nodesX <- attr(predict(qrf,x,nodes=TRUE),"nodes")
   rownames(nodesX) <- NULL
   nnodes <- max(nodesX)
   ntree <- ncol(nodesX)
   n <- nrow(x)
   valuesNodes  <- matrix(nrow=nnodes,ncol=ntree)
-  
+
   for (tree in 1:ntree){
       shuffledNodes <- nodesX[rank(ind <- sample(1:n,n)),tree]
       useNodes <- sort(unique(as.numeric(shuffledNodes)))
       valuesNodes[useNodes,tree] <- y[ind[match(useNodes,shuffledNodes )]]
   }
- 
-  
+
+
 
   qrf[["call"]] <- cl
   qrf[["valuesNodes"]] <- valuesNodes
 
   if(keep.inbag){
-      predictOOBNodes <- attr(predict(qrf,newdata=x,nodes=TRUE),"nodes")
-      rownames(predictOOBNodes) <- NULL
-      valuesPredict <- 0*predictOOBNodes
-      ntree <- ncol(valuesNodes)
-      for (tree in 1:ntree){
-          valuesPredict[,tree] <- valuesNodes[ predictOOBNodes[,tree],tree]  
+    
+    # create a prediction vector with same shape as predictOOBNodes
+    predictOOBNodes <- attr(predict(qrf,newdata=x,nodes=TRUE),"nodes")
+    rownames(predictOOBNodes) <- NULL
+    valuesPredict <- 0*predictOOBNodes
+    ntree <- ncol(valuesNodes)
+    valuesPredict[ qrf$inbag >0] <- NA
+
+
+    # for each tree and observation sample another observation of the same node
+    for (tree in 1:ntree){
+
+      is.oob <- qrf$inbag[,tree] == 0
+      n.oob <- sum(is.oob)
+
+      if(n.oob!=0) {
+      	
+	  y.oob  <- sapply(which(is.oob),
+		    function(i) {
+			    cur.node <- nodesX[i,tree]
+			    cur.y <- if(length(cur.y <- y[setdiff(which(nodesX[,tree]==cur.node),i)])!=0) {
+				    	sample(x = cur.y,size = 1)
+			             } else {
+			              	NA
+			    	     }			   
+			    return(cur.y)
+		       })
+          valuesPredict[is.oob, tree] <- y.oob
       }
-      valuesPredict[ qrf$inbag >0] <- NA
-      minoob <- min( apply(!is.na(valuesPredict),1,sum))
-      if(minoob<10) stop("need to increase number of trees for sufficiently many out-of-bag observations")
-      valuesOOB <- t(apply( valuesPredict,1 , function(x) sample( x[!is.na(x)], minoob)))
-      qrf[["valuesOOB"]] <- valuesOOB
+    }
+
+    minoob <- min( apply(!is.na(valuesPredict),1,sum))
+    if(minoob<10) stop("need to increase number of trees for sufficiently many out-of-bag observations")
+    valuesOOB <- t(apply( valuesPredict,1 , function(x) sample( x[!is.na(x)], minoob)))
+    qrf[["valuesOOB"]] <- valuesOOB
   }
   class(qrf) <- c("quantregForest","randomForest")
 
